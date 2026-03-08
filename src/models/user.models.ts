ef7c655b3eaa515e0290224ba048ff43c374b3ae
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2026-03-08 17:33:08 +0530

    Models

---
+++ src/models/user.models.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+}, { timestamps: true });
+
+export const User = model("User", userSchema);
+
+userSchema.pre("save", async function () {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+    return this.password === candidatePassword;
+}
